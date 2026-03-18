f6b3499bb12196bcd46fbb65bfc926a24cff5827
arshadvkb <arshadvkb118@gmail.com>
2026-03-18 11:07:41 +0530

    feat: Implement user authentication including signup, login, logout, and avatar upload.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -3,79 +3,95 @@ import userModel from "../models/user.model.js"
 import { genrateToken } from "../utils/tokens.js"
 import cloudinary from "../config/cloudinary.js";
 
-const signup=async(req,res)=>{
-    console.log( req.body);
+const signup = async (req, res) => {
+    console.log(req.body);
     console.log(req.file);
-    
-     const {name,email,password}=req.body
-   
-    
-     
 
-     if(!name || !email || !password){
-        return res.status(404).json({success:false,msg:"all fields are rquired"})
-     }
+    const { name, email, password } = req.body
+
+
+
+
+    if (!name || !email || !password) {
+        return res.status(404).json({ success: false, msg: "all fields are rquired" })
+    }
     try {
 
-        const existingUser= await userModel.findOne({email})
+        const existingUser = await userModel.findOne({ email })
 
-        if(existingUser){
-            return res.status(400).json({msg:"user with this email already exists",success:false})
+        if (existingUser) {
+            return res.status(400).json({ msg: "user with this email already exists", success: false })
         }
-        
-         const uploadResult = await cloudinary.uploader.upload(req.file.path, {
-           resource_type: "auto",
-           folder: "cube_collab",
-         });
-         const imageUrl=uploadResult.secure_url
-        
-         const salt=await bcrypt.genSalt(10)
-         const hashedPassword=await bcrypt.hash(password,salt)
 
-         const newUser=new userModel({
+        const uploadResult = await cloudinary.uploader.upload(req.file.path, {
+            resource_type: "auto",
+            folder: "cube_collab",
+        });
+        const imageUrl = uploadResult.secure_url
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new userModel({
             name,
             email,
-            password:hashedPassword,
-            avatar:imageUrl
-         })
+            password: hashedPassword,
+            avatar: imageUrl
+        })
 
-         await newUser.save()
-         await genrateToken(newUser._id,res)
-         
-         return res.status(200).json({success:true,msg:"Registerd successfuly"})
+        await newUser.save()
+        await genrateToken(newUser._id, res)
+
+        return res.status(200).json({ success: true, msg: "Registerd successfuly" })
 
 
 
     } catch (error) {
-        console.log("error in signup :" +error.message);
-        return res.status(500).json({success:false,msg:"error in signup",error:error.message})
-     }
-}
-
-
-const login=async(req,res)=>{
-    console.log(req.body);
-    try {
-        const {email,password}=req.body
-        if(!email || !password){
-            return res.status(404).json({success:false,msg:"all fields are rquired"})
-         }
-         const user=await userModel.findOne({email})
-         if(!user){
-            return res.status(404).json({success:false,msg:"user not found"})
-         }
-         const isPasswordValid=await bcrypt.compare(password,user.password)
-         if(!isPasswordValid){
-            return res.status(404).json({success:false,msg:"invalid password"})
-         }
-         await genrateToken(user._id,res)
-         return res.status(200).json({success:true,msg:"login successfuly"})
-    } catch (error) {
-        console.log("error in login :"+error.message);
-        return res.status(500).json({success:false,msg:"error in login",error:error.message})
-        
+        console.log("error in signup :" + error.message);
+        return res.status(500).json({ success: false, msg: "error in signup", error: error.message })
     }
 }
 
 
-export {signup,login}
+const login = async (req, res) => {
+    console.log(req.body);
+    try {
+        const { email, password } = req.body
+        if (!email || !password) {
+            return res.status(404).json({ success: false, msg: "all fields are rquired" })
+        }
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ success: false, msg: "user not found" })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            return res.status(404).json({ success: false, msg: "invalid password" })
+        }
+        await genrateToken(user._id, res)
+        return res.status(200).json({ success: true, msg: "login successfuly" })
+    } catch (error) {
+        console.log("error in login :" + error.message);
+        return res.status(500).json({ success: false, msg: "error in login", error: error.message })
+
+    }
+}
+
+const logout = async (req, res) => {
+
+
+    try {
+        res.cookie("token", "", { maxAge: 0 })
+        res.clearCookie("token")
+
+        return res.status(200).json({ success: true, msg: "logout successfuly" })
+    } catch (error) {
+        console.log("error in logout :" + error.message);
+        return res.status(500).json({ success: false, msg: "error in logout", error: error.message })
+    }
+}
+
+
+
+
+export { signup, login, logout }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 
-import {signup,login} from "../controllers/auth.controller.js"
+import {signup,login,logout } from "../controllers/auth.controller.js"
 import { upload } from "../middleware/multer.js";
 
 const authRouter =express.Router()
 
 authRouter.post("/signup", upload.single("file"),signup);
 authRouter.post("/login",login);
+authRouter.post("/logout",logout);
 
 export default authRouter
