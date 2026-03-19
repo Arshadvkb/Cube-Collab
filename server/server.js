c199f44785bedd384db930729ac016f33dc71546
arshadvkb <arshadvkb118@gmail.com>
2026-03-19 15:50:08 +0530

    feat: implement initial server setup with Express, API routes, database connection, and Swagger documentation.

--- server/server.js
+++ server/server.js
@@ -17,7 +17,10 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 
 
