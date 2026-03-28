8d6f1ce93d38c2da7841e7c1ed1eede3624f1b8c
arshadvkb <arshadvkb118@gmail.com>
2026-03-28 14:30:41 +0530

    added socket connection

---
+++ client/src/lib/socket_client.js
@@ -0,0 +1,12 @@
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5000");
+socket.on("connect", () => {
+  console.log("Connected:", socket.id);
+});
+
+socket.on("connect_error", (err) => {
+  console.log("Connection error:", err.message);
+});
+
+export default socket
---
+++ server/utils/socket.js
@@ -0,0 +1,26 @@
+import { Server } from "socket.io";
+
+let io;
+
+export const initSocket = (server) => {
+  io = new Server(server, {
+    cors: { 
+        origin: ["http://localhost:5173"],
+        credentials: true,
+    },
+  });   
+    io.on("connection", (socket) => {
+        console.log("A user connected: " + socket.id);
+
+        socket.on("disconnect", () => {
+            console.log("A user disconnected: " + socket.id);
+        });
+    });
+}
+
+export const getIo = () => {
+    if (!io) {
+        throw new Error("Socket.io not initialized");
+    }   
+    return io;
+}
